96d9953d007ce7ff61d42c5b55e5a58e84b5d11b
Lazizbek-Narzullayev <150608982+Lazizbek-Narzullayev@users.noreply.github.com>
2024-07-12 12:18:03 +0500

    Add files via upload

---
+++ East Telecom/script.js
@@ -0,0 +1,10 @@
+const lupa=document.querySelector("#lupa");
+let search__blog=document.querySelector("#search__div");
+const text=document.querySelector(".header__text")
+const bottom=document.querySelector(".header__container")
+lupa=addEventListener("click",search__element)
+function search__element(){
+
+    search__blog.style.opacity=1;
+    bottom.style.margin.top="100px";
+}
